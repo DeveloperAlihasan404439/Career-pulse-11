f1308d3d0fa8f1dcfb264c6b07afb8285202b2f7
alihasanlitonislam <alihasan404439@gmail.com>
2023-11-06 03:35:59 +0600

    user post job in access the mongodb

--- src/page/MyPostedJobs/MyPostedJobs.jsx
+++ src/page/MyPostedJobs/MyPostedJobs.jsx
@@ -1,9 +1,25 @@
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Authentication/AuthProvider/AuthProvider";
+import BannerShared from "../../shared/BannerShared/BannerShared";
+
 const MyPostedJobs = () => {
-    return (
-        <div>
-            <h1>my Posted Jobs</h1>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/catagory?email=${user?.email}`)
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, [user]);
+  return (
+    <div className="bg-base-200">
+        <BannerShared url='https://i.ibb.co/1q5kjTT/Web-Development-Manager.png' title="My Posted Jobs" catagory="My Jobs" />
+      <div className="overflow-x-auto w-11/12 mx-auto min-h-screen">
+        
+      </div>
+    </div>
+  );
 };
 
 export default MyPostedJobs;
